a31f1bd50f439f57341290ea729e1d9f0f17c129
truebrooklynn <72233694+truebrooklynn@users.noreply.github.com>
2024-09-03 13:36:04 -0400

    Added art porfolio content

---
+++ sketchpad/script.js
@@ -0,0 +1,239 @@
+const artPortfolioImages = [
+  {
+    link: 'https://drive.google.com/thumbnail?id=1POwF5HqlG2SFaTQ-jg7qyJEylr_VbV2o&sz=w1000',
+    title: 'Oasis in the Desert',
+    description: 'Sketch of a male and female centaur, smiling, in vivid colors.',
+    tagMedium: 'traditional',
+    tagCategory: 'abstract',
+    tagSubjectMain: 'person'
+  },
+  {
+    link: 'https://drive.google.com/thumbnail?id=1bmWukOlxLxACTOtnL_VEGs_mSuKV91Dh&sz=w1000',
+    title: 'Green Thumb',
+    description: '',
+    tagMedium: 'traditional',
+    tagCategory: 'abstract',
+    tagSubjectMain: ''
+  },
+  {
+    link: 'https://drive.google.com/thumbnail?id=1c9irZ8yqvxDCwWVt8Ua1qM7aW9Xs5Rfo&sz=w1000',
+    title: 'Orange Cat with Yarn',
+    description: '',
+    tagMedium: 'traditional',
+    tagCategory: 'animal',
+    tagSubjectMain: 'cat'
+  },
+  {
+    link: 'https://drive.google.com/thumbnail?id=1bfYOkDvxDhJd3Fdknf7HlJPe0t67-0_8&sz=w1000',
+    title: 'Squirrel with Single Rose',
+    description: '',
+    tagMedium: 'traditional',
+    tagCategory: 'animal',
+    tagSubjectMain: 'squirrel'
+  },
+  {
+    link: 'https://drive.google.com/thumbnail?id=1bSMtFm4E_mRltwzmgH-MNRtRfMG2_kaL&sz=w1000',
+    title: 'Kittens in Bed',
+    description: '',
+    tagMedium: 'traditional',
+    tagCategory: 'animal',
+    tagSubjectMain: 'cat'
+  },
+  {
+    link: 'https://drive.google.com/thumbnail?id=1c9SlEHtDMliFYgeeypUheWRneirX5Df5&sz=w1000',
+    title: 'Cardinals on Spring Buds',
+    description: '',
+    tagMedium: 'traditional',
+    tagCategory: 'animal',
+    tagSubjectMain: 'bird'
+  },
+  {
+    link: 'https://drive.google.com/thumbnail?id=1tm6Re76O6GYRdKK9e9pFIdglo2Xy3ZMi&sz=w1000',
+    title: 'Night Vision',
+    description: 'Sketch of an animal in the dark.',
+    tagMedium: 'digital',
+    tagCategory: 'animal',
+    tagSubjectMain: ''
+  },
+  {
+    link: 'https://drive.google.com/thumbnail?id=1EsLnUCl_2ArMXyCKpxJPgqsnAz_3cp2S&sz=w1000',
+    title: 'Pink Whale',
+    description: 'Sketch of a cartoon whale in pink, smiling.',
+    tagMedium: 'digital',
+    tagCategory: 'animal',
+    tagSubjectMain: 'whale'
+  },
+  {
+    link: 'https://drive.google.com/thumbnail?id=1plXvIluYP4XWtGVLEh8rG8_tQS1Nzg2T&sz=w1000',
+    title: 'Walking Her Dog',
+    description: 'Sketch of a woman walking a dog.',
+    tagMedium: 'digital',
+    tagCategory: 'full-body',
+    tagSubjectMain: 'person'
+  },
+  {
+    link: 'https://drive.google.com/thumbnail?id=1IphJ3HTx8DY1FH4IEDlzKS00nlcVfmui&sz=w1000',
+    title: 'Two Breads and Four Plates',
+    description: '',
+    tagMedium: 'digital',
+    tagCategory: 'landscape',
+    tagSubjectMain: ''
+  },
+  {
+    link: 'https://drive.google.com/thumbnail?id=1zV3j1Y-YD2C_uBNblRYRG3UPdPpMpo7F&sz=w1000',
+    title: 'Back to School',
+    description: 'Portrait of a woman with black hair, smiling, wearing a burgundy crew neck sweater.',
+    tagMedium: 'digital',
+    tagCategory: 'portrait',
+    tagSubjectMain: 'person'
+  },
+  {
+    link: 'https://drive.google.com/thumbnail?id=1fbWixE0I5MzyjXw_cmFtONLi1sk_XbwE&sz=w1000',
+    title: 'Half Up, Half Down Waves',
+    description: 'Portrait of a woman with brown hair in a half up, half down hairstyle.',
+    tagMedium: 'digital',
+    tagCategory: 'portrait',
+    tagSubjectMain: 'person'
+  },
+  {
+    link: 'https://drive.google.com/thumbnail?id=1kNNriGFUXg60B9hZoO3kpa-_PJbfEW4d&sz=w1000',
+    title: 'Venus',
+    description: 'Portrait of a woman with blue hair, a pink animal, and a yellow crescent moon.',
+    tagMedium: 'traditional',
+    tagCategory: 'portrait',
+    tagSubjectMain: 'person'
+  },
+  {
+    link: 'https://drive.google.com/thumbnail?id=1n2DlF2uG88lqYtYrBxRQ0-G5XLNozNzK&sz=w1000',
+    title: 'Blue Prince',
+    description: 'Portrait of a man with light blonde hair, green collared shirt, a small gold crown above his head, frowning.',
+    tagMedium: 'digital',
+    tagCategory: 'portrait',
+    tagSubjectMain: 'person'
+  },
+  {
+    link: 'https://drive.google.com/thumbnail?id=1fwdtKyz77reVsF8408smokbd7FHOHaWU&sz=w1000',
+    title: 'Fur and Tassels',
+    description: '',
+    tagMedium: 'traditional',
+    tagCategory: 'fashion',
+    tagSubjectMain: 'person'
+  },
+  {
+    link: 'https://drive.google.com/thumbnail?id=1zE4KSVzkwYJ8Ylwiyil7rDVagL8dtMTj&sz=w1000',
+    title: 'Everything in Between',
+    description: '',
+    tagMedium: 'traditional',
+    tagCategory: 'fashion',
+    tagSubjectMain: 'person'
+  },
+  {
+    link: 'https://drive.google.com/thumbnail?id=1ChunXeF9qiMBdFElklM_YyHa9ok8Jip4&sz=w1000',
+    title: 'Arriving at the Party',
+    description: '',
+    tagMedium: 'traditional',
+    tagCategory: 'fashion',
+    tagSubjectMain: 'person'
+  },
+  {
+    link: 'https://drive.google.com/thumbnail?id=1qIY7_HUJguo3fHxbQmYGv68000p9uFlz&sz=w1000',
+    title: 'Bold',
+    description: '',
+    tagMedium: 'traditional',
+    tagCategory: 'fashion',
+    tagSubjectMain: 'person'
+  },
+  {
+    link: 'https://drive.google.com/thumbnail?id=1bVLx8crzqv4m9RlGkCMzGBnlWk3BW3q0&sz=w1000',
+    title: 'Red Roses and Green Leaves',
+    description: '',
+    tagMedium: 'traditional',
+    tagCategory: 'foliage-and-flora',
+    tagSubjectMain: ''
+  },
+  {
+    link: 'https://drive.google.com/thumbnail?id=1bVT0nA7uEl215GdwK6fC7FGviFsxLjaw&sz=w1000',
+    title: 'Fireflies at Sunset',
+    description: '',
+    tagMedium: 'traditional',
+    tagCategory: 'landscape',
+    tagSubjectMain: ''
+  },
+  {
+    link: 'https://drive.google.com/thumbnail?id=1c4sROj2k6o8AzXKaWw3AzKVZMJDGQpLV&sz=w1000',
+    title: 'Roses and Women',
+    description: 'Portrait of two women with a rose.',
+    tagMedium: 'traditional',
+    tagCategory: 'portrait',
+    tagSubjectMain: 'person'
+  },
+  {
+    link: 'https://drive.google.com/thumbnail?id=1bdRlpgBuPM1eryMIDTGmb8T46t4TlDkA&sz=w1000',
+    title: 'Arctic Wolf',
+    description: '',
+    tagMedium: 'traditional',
+    tagCategory: 'portrait',
+    tagSubjectMain: ''
+  },
+  {
+    link: 'https://drive.google.com/thumbnail?id=1bMN9ZL8BYtF2L9aLNLDeFDb6aL5Arz_x&sz=w1000',
+    title: 'Flowing Waves',
+    description: 'Portrait of a woman with flowing long hair.',
+    tagMedium: 'traditional',
+    tagCategory: 'portrait',
+    tagSubjectMain: 'person'
+  },
+  {
+    link: 'https://drive.google.com/thumbnail?id=1bQilQBtkIQjlDl-oYh2lLUF2pju9b_dd&sz=w1000',
+    title: 'High Ponytail Waves',
+    description: 'Portrait of a woman with a high ponytail.',
+    tagMedium: 'traditional',
+    tagCategory: 'portrait',
+    tagSubjectMain: 'person'
+  },
+];
+
+const root = document.getElementById('galleryRoot');
+let defaultFilter = "none";
+
+function filterSelection(selectedFilter){
+  // clear the page
+  root.replaceChildren();
+
+  for (let i = 0; i < artPortfolioImages.length; i++) {
+    if ((selectedFilter === "none") || (artPortfolioImages[i].tagMedium === selectedFilter)) {
+      var imageDiv = document.createElement('div');
+      imageDiv.classList.add('oneImage');
+    
+      // Render image heading
+      var mainHeading = document.createElement('h1');
+      mainHeading.appendChild(document.createTextNode(artPortfolioImages[i].title));
+      imageDiv.appendChild(mainHeading);
+    
+      // Render image
+      var singleImage = document.createElement('img');
+      singleImage.src = artPortfolioImages[i].link;
+      imageDiv.appendChild(singleImage);
+    
+      // Render description
+      var description = document.createElement('p');
+      description.appendChild(document.createTextNode(artPortfolioImages[i].description));
+      imageDiv.appendChild(description);
+      
+      root.appendChild(imageDiv);
+    }  
+  };
+}
+
+filterSelection(defaultFilter)
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("galleryFilters");
+var filterButtons = document.getElementById("galleryFilters").getElementsByClassName("filterButton");
+for (var i = 0; i < filterButtons.length; i++) {
+  filterButtons[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
